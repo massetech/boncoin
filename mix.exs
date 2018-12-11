5dc60bd42cb1552d859f352d5dc5371d334eedb1
Thibaut <bitocreator@gmail.com>
2018-12-11 22:49:03 +0630

    fixes

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Boncoin.Mixfile do
 
   def project do
     [ app: :boncoin,
-      version: "1.4.4",
+      version: "1.4.5",
       elixir: "~> 1.7.3",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
